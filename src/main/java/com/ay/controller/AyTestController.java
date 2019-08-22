ae8843f8f8f8ea9ba7426b593d449c574e79503d
liuxigui <484777573@qq.com>
2019-08-23 01:23:04 +0800

    整合mybatis，但是还有错，没整合好

--- src/main/java/com/ay/controller/AyTestController.java
+++ src/main/java/com/ay/controller/AyTestController.java
@@ -1,16 +1,33 @@
 package com.ay.controller;
 
+import com.ay.model.AyUser;
+import com.ay.service.AyUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import javax.jws.WebParam;
+import java.util.List;
 
 
 @Controller
-@RequestMapping("/test")
+@RequestMapping("/user")
 public class AyTestController {
 
-    @RequestMapping("/a")
-    public String hello(){
-        System.out.println("请求进来了");
+    @Resource
+    private AyUserService ayUserService;
+
+
+    @RequestMapping("/findAll")
+    public String findAll(Model model){
+        System.out.println("11111");
+
+        List<AyUser>ls= ayUserService.findAll();
+
+        System.out.println(ls);
+
         return "hello";
     }
 }
