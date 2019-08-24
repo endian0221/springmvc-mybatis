75d5493c793c5649118e1f4662d11c0ce8e80bf2
484777573@qq.com <370826lxg>
2019-08-24 17:39:37 +0800

    整合ssm完毕，junit完毕，log4j完毕，发现缺点：请求一次，但是后台执行了2次有空在解决

--- src/main/java/com/ay/controller/AyTestController.java
+++ src/main/java/com/ay/controller/AyTestController.java
@@ -2,29 +2,24 @@ package com.ay.controller;
 
 import com.ay.model.AyUser;
 import com.ay.service.AyUserService;
-import com.ay.service.impl.AyUserServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
-import javax.jws.WebParam;
 import java.util.List;
 
 
 @Controller
-@RequestMapping(value = "user")
+@RequestMapping(value = "/user")
 public class AyTestController {
 
     @Resource
     private AyUserService ayUserService;
 
-
     @RequestMapping(value = "/findAll",method = RequestMethod.GET)
     public String findAll(Model model){
-        System.out.println("111111");
         List<AyUser> ayUserList = ayUserService.findAll();
         for(AyUser ayUser : ayUserList){
             System.out.println("id: " + ayUser.getId());
